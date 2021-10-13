f87af0eab3a646187d8336f82afceb5cef61037d
Jonathan Sick <jsick@lsst.org>
2021-10-13 11:19:15 -0400

    Initial port of JupyterClient and mock
    This is ported from mobu and modified to work with httpx, websockets,
    and respx instead of aiohttp and aioresponses.
    
    Both the JupyterClient and the JupyterLabSession is now a contexts so
    that they automatically close their connections.
    
    The JupyterLabSession is now where python execution happens, rather than
    in the Jupyter Client, as done in mobu.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,21 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pytest
+import respx
 from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
 
 from noteburst import main
+from tests.support.cachemachine import mock_cachemachine
+from tests.support.jupyter import mock_jupyter
 
 if TYPE_CHECKING:
     from typing import AsyncIterator
 
     from fastapi import FastAPI
+
+    from tests.support.cachemachine import MockCachemachine
+    from tests.support.jupyter import MockJupyter
 
 
 @pytest.fixture
@@ -32,3 +38,15 @@ async def client(app: FastAPI) -> AsyncIterator[AsyncClient]:
     """Return an ``httpx.AsyncClient`` configured to talk to the test app."""
     async with AsyncClient(app=app, base_url="https://example.com/") as client:
         yield client
+
+
+@pytest.fixture
+async def cachemachine(respx_mock: respx.Router) -> MockCachemachine:
+    """Mock the cachemachine API."""
+    return mock_cachemachine(respx_mock)
+
+
+@pytest.fixture
+def jupyter(respx_mock: respx.Router) -> MockJupyter:
+    """Mock out JupyterHub/Lab API."""
+    return mock_jupyter(respx_mock)
