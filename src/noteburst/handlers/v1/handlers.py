abd9180b914eaa9aeaaf085648aa90b8e0d3428f
Jonathan Sick <jsick@lsst.org>
2022-02-28 18:09:27 -0500

    Add additional docs for /v1/ API

--- src/noteburst/handlers/v1/handlers.py
+++ src/noteburst/handlers/v1/handlers.py
@@ -2,7 +2,7 @@
 
 import structlog
 from arq.jobs import JobStatus
-from fastapi import APIRouter, Depends, Request, Response
+from fastapi import APIRouter, Depends, Query, Request, Response
 from safir.dependencies.logger import logger_dependency
 
 from noteburst.dependencies.arqpool import ArqQueue, arq_dependency
@@ -15,10 +15,7 @@ v1_router = APIRouter(tags=["v1"])
 
 @v1_router.post(
     "/notebooks/",
-    description=(
-        "Execute a Jupyter notebook, asynchronously via a pool of JupyterLab "
-        "instances."
-    ),
+    summary="Submit a notebook for execution",
     status_code=202,
     response_model=NotebookResponse,
     response_model_exclude_none=True,
@@ -31,6 +28,33 @@ async def post_nbexec(
     logger: structlog.BoundLogger = Depends(logger_dependency),
     arq_queue: ArqQueue = Depends(arq_dependency),
 ) -> NotebookResponse:
+    """Submits a notebook for execution. The notebook is executed
+    asynchronously via a pool of JupyterLab (Nublado) instances.
+
+    ## Configuring how the notebook is run
+
+    The JupyterLab kernel can be set via the optional `kernel_name` field.
+    The default kernel is `LSST`, which is a Python 3 kernel that includes
+    the full Rubin Python environment
+    ([rubinenv](https://github.com/conda-forge/rubinenv-feedstock)).
+
+    Note that you cannot specify some aspects of the JupyterLab pod that
+    the notebook is run on:
+
+    - The user identity (a generic account is used)
+    - The Nublado version
+    - The machine size
+
+    ## Getting the notebook status and result
+
+    The JSON response body includes a `self_url` field (the same value is also
+    available in the `Location` response header). You can send a
+    `GET` request to this URL to get metadata about the execution job
+    and (if available) the notebook (`ipynb`) result. See
+    [`GET /v1/notebooks/{job_id}`](
+    #/v1/get_nbexec_job_noteburst_v1_notebooks__job_id__get) for more
+    information.
+    """
     logger.debug("Enqueing a nbexec task")
     job_metadata = await arq_queue.enqueue(
         "nbexec",
@@ -47,7 +71,7 @@ async def post_nbexec(
 
 @v1_router.get(
     "/notebooks/{job_id}",
-    description="Get information about a notebook execution job.",
+    summary="Get information about a notebook execution job.",
     response_model=NotebookResponse,
     response_model_exclude_none=True,
 )
@@ -55,11 +79,47 @@ async def get_nbexec_job(
     *,
     job_id: str,
     request: Request,
-    source: bool = False,
-    result: bool = True,
+    source: bool = Query(
+        False,
+        title="Include source ipynb",
+        description=(
+            "If set to true, the `source` field will include the JSON-encoded "
+            "content of the source ipynb notebook."
+        ),
+    ),
+    result: bool = Query(
+        True,
+        title="Include the result",
+        description=(
+            "If set to true and the notebook run is complete, the response "
+            "includes the executed notebook and metadata about the run."
+        ),
+    ),
     logger: structlog.BoundLogger = Depends(logger_dependency),
     arq_queue: ArqQueue = Depends(arq_dependency),
 ) -> NotebookResponse:
+    """Provides information about a notebook execution job, and the result
+    (if available).
+
+    ## Information from a completed notebook
+
+    Many response fields are only included when the result is available
+    (the `status` field is `complete`):
+
+    - `ipynb` (the JSON-included executed Jupyter notebook)
+    - `start_time`
+    - `finish_time`
+    - `success`
+
+    If you do not require these fields, regardless of the `status`, you can
+    set the `result=false` URL query parameter. This speeds up the response
+    slightly.
+
+    ## Toggling inclusion of the source notebook
+
+    If you require the notebook that was originally submitted, set the
+    URL query parameter `source=true`.
+    """
     job_metadata = await arq_queue.get_job_metadata(job_id)
     if result and job_metadata.status == JobStatus.complete:
         job_result = await arq_queue.get_job_result(job_id)
