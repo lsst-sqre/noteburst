49a56f58c0a902a1ebdd98275fe8c08fd4d7057c
Jonathan Sick <jsick@lsst.org>
2022-03-14 15:35:02 -0400

    Use Pydantic fields to improve OpenAPI docs

--- src/noteburst/handlers/v1/models.py
+++ src/noteburst/handlers/v1/models.py
@@ -8,9 +8,20 @@ from typing import Any, Dict, Optional, Union
 
 from arq.jobs import JobStatus
 from fastapi import Request
-from pydantic import AnyHttpUrl, BaseModel
+from pydantic import AnyHttpUrl, BaseModel, Field
 
 from noteburst.dependencies.arqpool import JobMetadata, JobResult
+
+kernel_name_field = Field(
+    "LSST",
+    title="The name of the Jupyter kernel the kernel is executed with",
+    example="LSST",
+    description=(
+        "The default kernel, LSST, contains the full Rubin Python "
+        "environment, [rubinenv](https://anaconda.org/conda-forge/rubin-env), "
+        "which includes the LSST Science Pipelines."
+    ),
+)
 
 
 class NotebookResponse(BaseModel):
@@ -18,47 +29,50 @@ class NotebookResponse(BaseModel):
     result and source notebooks.
     """
 
-    job_id: str
-    """The job ID."""
+    job_id: str = Field(title="The job ID")
 
-    kernel_name: str
-    """Name of the kernel the notebook is executed with."""
+    kernel_name: str = kernel_name_field
 
-    enqueue_time: datetime
-    """Time when the job was added to the queue (UTC)."""
+    enqueue_time: datetime = Field(
+        title="Time when the job was added to the queue (UTC)"
+    )
 
-    status: JobStatus
-    """The current status of the notebook execution job."""
+    status: JobStatus = Field(
+        title="The current status of the notebook execution job"
+    )
 
-    self_url: AnyHttpUrl
-    """The URL of this resource."""
+    self_url: AnyHttpUrl = Field(title="The URL of this resource")
 
-    source: Optional[str] = None
-    """The content of the source ipynb file (JSON-encoded string)."""
+    source: Optional[str] = Field(
+        None,
+        title="The content of the source ipynb file (JSON-encoded string)",
+        description="This field is null unless the source is requested.",
+    )
 
-    start_time: Optional[datetime] = None
-    """Time when the job started (UTC).
+    start_time: Optional[datetime] = Field(
+        None,
+        title="Time when the notebook execution started (UTC)",
+        description="This field is present if the result is available.",
+    )
 
-    This field is present if the result is available.
-    """
+    finish_time: Optional[datetime] = Field(
+        None,
+        title="Time when the notebook execution completed (UTC)",
+        description="This field is present only if the result is available.",
+    )
 
-    finish_time: Optional[datetime] = None
-    """Time when the job completed (UTC).
+    success: Optional[bool] = Field(
+        None,
+        title="Whether the execution was successful or not",
+        description="This field is present if the result is available.",
+    )
 
-    This field is present if the result is available.
-    """
-
-    success: Optional[bool] = None
-    """Whether the execution was successful or not.
-
-    This field is present if the result is available.
-    """
-
-    ipynb: Optional[str] = None
-    """The contents of the executed Jupyter notebook.
-
-    This field is present if the result is available.
-    """
+    ipynb: Optional[str] = Field(
+        None,
+        title="The contents of the executed Jupyter notebook",
+        description="The ipynb is a JSON-encoded string. This field is "
+        "present if the result is available.",
+    )
 
     @classmethod
     async def from_job_metadata(
@@ -86,14 +100,15 @@ class NotebookResponse(BaseModel):
 class PostNotebookRequest(BaseModel):
     """The ``POST /notebooks/`` request body."""
 
-    ipynb: Union[str, Dict[str, Any]]
-    """The contents of a Jupyter notebook. If a string, the conttent is parsed
-    as JSON. Alternatively, the content can be submitted pre-pared as an
-    object.
-    """
+    ipynb: Union[str, Dict[str, Any]] = Field(
+        ...,
+        title="The contents of a Jupyter notebook",
+        description="If a string, the content is parsed as JSON. "
+        "Alternatively, the content can be submitted pre-parsed as "
+        "an object.",
+    )
 
-    kernel_name: str = "LSST"
-    """The name of the Jupyter kernel to execute this by."""
+    kernel_name: str = kernel_name_field
 
     def get_ipynb_as_str(self) -> str:
         if isinstance(self.ipynb, str):
