e19eb3161c37a78805f33631a4c2fb38708b6a31
Jonathan Sick <jsick@lsst.org>
2023-05-22 16:01:55 -0400

    Add NOTEBURST_JUPYTERHUB_PATH_PREFIX
    This environment variable controls what path the JupyterHub/JupyterLab
    service is running on.

--- src/noteburst/config.py
+++ src/noteburst/config.py
@@ -86,6 +86,12 @@ class Config(BaseSettings):
     This is used for creating URLs to services, such as JupyterHub.
     """
 
+    jupyterhub_path_prefix: str = Field(
+        "/nb/",
+        env="NOTEBURST_JUPYTERHUB_PATH_PREFIX",
+        description="The path prefix for the JupyterHub service.",
+    )
+
     gafaelfawr_token: SecretStr = Field(env="NOTEBURST_GAFAELFAWR_TOKEN")
     """This token is used to make an admin API call to Gafaelfawr to get a
     token for the user.
