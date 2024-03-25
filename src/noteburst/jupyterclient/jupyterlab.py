6dbc12558b656a47328c4ff00ddf9c152037bddf
Jonathan Sick <jsick@lsst.org>
2024-03-25 11:11:52 -0400

    Disable read timeout on notebook execution request
    A lot of user notebooks are running into the ReadTimeout error on
    notebook execution. We can mitigate this as much as possible on our end
    by disabling read timeouts.

--- src/noteburst/jupyterclient/jupyterlab.py
+++ src/noteburst/jupyterclient/jupyterlab.py
@@ -14,7 +14,7 @@ from uuid import uuid4
 
 import httpx
 import websockets
-from httpx import Cookies
+from httpx import Cookies, Timeout
 from pydantic import BaseModel, Field
 from structlog import BoundLogger
 from websockets.client import WebSocketClientProtocol
@@ -727,10 +727,14 @@ class JupyterClient:
         if self._lab_xsrf:
             headers["X-XSRFToken"] = self._lab_xsrf
         try:
+            # The timeout is designed to catch issues connecting to JupyterLab
+            # but to wait as long as possible for the notebook itself
+            # to execute.
             r = await self.http_client.post(
                 exec_url,
                 content=json.dumps(notebook).encode("utf-8"),
                 headers=headers,
+                timeout=Timeout(5.0, read=None),
             )
             r.raise_for_status()
         except httpx.ReadTimeout as e:
